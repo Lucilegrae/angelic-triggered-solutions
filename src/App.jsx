9f460562acf79e152a1c41b514f9ab5a55420e5d
Lucilegrae <pnovontony@yahoo.com>
2026-04-02 00:43:33 +0200

    Update App.jsx with aura overlays and showcase components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
-import SupabaseTest from './SupabaseTest'
+import React from "react";
+import Banner from "./Banner";
+import Navigation from "./Navigation";
+import Footer from "./Footer";
+import AffirmationsList from "./AffirmationsList";
+import ProjectsList from "./ProjectsList";
+import StakeholdersList from "./StakeholdersList";
 
-export default function App() {
+function App() {
   return (
-    <div className="container">
-      <h1>Angelic Triggered Solutions</h1>
-      <SupabaseTest />
+    <div className="app-container aura-overlay">
+      <Navigation />
+      <Banner />
+      <main>
+        <AffirmationsList />
+        <ProjectsList />
+        <StakeholdersList />
+      </main>
+      <Footer />
     </div>
-  )
+  );
 }
+
+export default App;
